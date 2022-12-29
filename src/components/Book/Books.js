06b9b046ec4e8e9536ca6ab089b2017f74bc27df
Ravichandra Patil <105003257+Ravip925@users.noreply.github.com>
2022-12-29 17:31:24 +0530

    Update Books.js

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -1,8 +1,31 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { publicRequest } from "../../requestMethods";
+import { mobile } from "../../responsive";
 import Footer from "../Footer";
 import Book from "./Book";
 import "./book.css";
+
+const Spinner = styled.div`
+  border: 10px solid #f3f3f3;
+  border-top: 10px solid #3498db;
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  animation: spin 1s linear infinite;
+  position: absolute;
+  top: 40%;
+  left: 47%;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  ${mobile({ left:"43%" })}
+`;
 
 const fetchHandler = async () => {
   return await publicRequest.get("/books").then((res) => res.data);
@@ -17,17 +40,23 @@ const Books = () => {
 
   return (
     <>
-      <div className="main">
-        <ul>
-          {books &&
-            books.map((book, index) => (
-              <li key={index}>
-                <Book book={book} />
-              </li>
-            ))}
-        </ul>
-      </div>
-      <Footer />
+      {books ? (
+        <>
+          <div className="main">
+            <ul>
+              {books &&
+                books.map((book, index) => (
+                  <li key={index}>
+                    <Book book={book} />
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 };
